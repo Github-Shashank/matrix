2c011ee6153832e8f2e45f675ea8ec32b7e28c9a
Github-Shashank <116636799+Github-Shashank@users.noreply.github.com>
2025-12-01 16:43:04 +0530

    Added feature of mulitplication
    This feature allows you to multiply to two matrix with each other or scalar multiplication of matrix

--- matrix.cpp
+++ matrix.cpp
@@ -13,21 +13,20 @@ public:
     bool check(const vector<vector<int>>& D) {
         if (D.empty()) return false;
 
-        int c = D[0].size();
-        for (const vector<int>& v : D) {
-            if (v.size() != c)
-                return false;
-        }
-
         rows = D.size();
-        cols = c;
+        cols = D[0].size();
+        for (const vector<int>& v : D) {
+            if (v.size() != cols){
+                return false;
+            }
+        }
 
         ord[0] = rows;
         ord[1] = cols;
 
         return true;
     }
-
+    void setOrder(int& r,int& c) const;
     Matrix() = default;
 
     Matrix(const vector<vector<int>>& v) {
@@ -51,7 +50,11 @@ public:
     bool operator==(const Matrix& other);
     Matrix operator+(const Matrix& other);
     Matrix operator-(const Matrix& other);
-    // Matrix operator*(const Matrix& other);
+    Matrix operator*(const Matrix& other);
+    Matrix operator*(const int& num)const;
+    friend Matrix operator*(int num, const Matrix& m) {
+        return m * num;
+    }
 };
 
 pair<int,int> Matrix::getOrd() {
@@ -60,6 +63,11 @@ pair<int,int> Matrix::getOrd() {
 
 bool Matrix::operator==(const Matrix& other){
     return data == other.data;
+}
+
+void Matrix::setOrder(int &r, int &c) const{ 
+    r = rows;
+    c = cols;
 }
 
 // Addition
@@ -91,29 +99,53 @@ Matrix Matrix::operator-(const Matrix& other) {
 }
 
 // Multiplication
-// Matrix Matrix::operator*(const Matrix& other) {
-//     if (cols != other.rows) {
-//         vector<vector<int>> result(rows, vector<int>(other.cols));
-        
-//     }
-// }
+Matrix Matrix::operator*(const Matrix& other) {
+    int x,y,i,j;
+    this->setOrder(x,y);
+    other.setOrder(i,j);
+    if (cols != other.rows) {
+        throw runtime_error("Matrix dimensions do not match for multiplication");
+    }
+    vector<vector<int>> result(x,vector<int>(j));
+
+    for (int self = 0; self < x; self++) {
+        for (int othr = 0; othr < j; othr++) {
+            for (int dot = 0; dot < y; dot++) {
+                result[self][othr] += data[self][dot]*other.data[dot][othr];
+            }
+        }
+    }
+    return Matrix(result);
+}
+
+Matrix Matrix::operator*(const int& num) const {
+    int x,y;
+    this->setOrder(x,y);
+    vector<vector<int>> result(x,vector<int>(y));
+    for (int r = 0; r < x; r++) {
+        for (int c = 0; c < y; c++) {
+            result[r][c] = data[r][c]*num;
+        }
+    }
+    return Matrix(result);
+}
 
 int main() {
     cout << "Code Started\n";
 
     vector<vector<int>> v = {
         {1,2,3},
-        {4,5,6},
-        {7,8,9}
+        {4,5,6}//,
+        // {7,8,9}
     };
 
     Matrix m(v);
     m.display();
 
     vector<vector<int>> b = {
-        {7,8,9},
-        {4,5,6},
-        {1,2,3}
+        {7,8},
+        {4,5},
+        {1,2}
     };
 
     Matrix a(b);
@@ -126,11 +158,25 @@ int main() {
     cout << "Matrix Order A: " << c.first << " x " << c.second << endl;
 
     Matrix d;
-    cout << "Sum" << endl;
-    d=a+m;
+    // cout << "Sum" << endl;
+    // d=a+m;
+    // d.display();
+    // cout << "Difference" << endl;
+    // d=a-m;
+    // d.display();
+    cout << endl << "a" << endl;
+    a.display();
+    cout << "m" << endl;
+    m.display();
+    cout << "Multiply" << endl;
+    d = a*m;
     d.display();
-    cout << "Difference" << endl;
-    d=a-m;
+    
+    cout << "a" << endl;
+    a.display();
+    d  = a*5;
+    d.display();
+    d = 5 * a;
     d.display();
 
     return 0;
